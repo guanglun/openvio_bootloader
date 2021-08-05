aa020287a1c57f5a5cb7d7a49485320faf6d3756
guanglun <272073020@qq.com>
2021-08-05 11:16:10 +0800

    update

---
+++ Bootloader/iap.c
@@ -0,0 +1,11 @@
+#include "iap.h"
+
+int iap_jump_to(uint32_t address)
+{
+    if (((*(uint32_t *)address) & 0x20000000) == 0x20000000)
+    {
+        system_jump_to(address);
+        return 1;
+    }
+    return 0;
+}
---
+++ Bootloader/iap.h
@@ -0,0 +1,10 @@
+#ifndef __IAP_H__
+#define __IAP_H__
+
+#include "main.h"
+#include "cmsis_os.h"
+#include "usart.h"
+#include "usb_device.h"
+#include "gpio.h"
+
+#endif
